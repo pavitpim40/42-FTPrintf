7181e62e722f72eb5036dfdc9d64ce3ef1eb3f22
Pavit Pimchanagul <pavitpim@gmail.com>
2023-03-23 19:46:08 +0700

    init repo

---
+++ ft_printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppimchan <ppimchan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/23 19:41:23 by ppimchan          #+#    #+#             */
+/*   Updated: 2023/03/23 19:42:41 by ppimchan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include	<stdlib.h>
+# include	<limits.h>
+# include	<stdarg.h>
+# include	<unistd.h>
+# include	<stdio.h>
+
+int		ft_printf(const char *format, ...);
+
+#endif
